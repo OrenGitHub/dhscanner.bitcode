198e6323933af5a1ed05e260056eb5369e11b5d9
OrenGitHub <tuna.is.good.for.you@gmail.com>
2026-03-06 12:25:56 +0200

    Add fully compatible fqn

--- src/Fqn.hs
+++ src/Fqn.hs
@@ -6,24 +6,112 @@ module Fqn
 
 where
 
+-- project imports
+import Location
+import qualified Token
+
 -- general imports
 import Data.Aeson
 import GHC.Generics
 
--- | Fully qualified name
-data Fqn = Fqn { content :: String } deriving ( Show, Eq, Ord, Generic, ToJSON, FromJSON )
+-- |
+--
+-- * Fully qualified name
+-- * Resolution at the /file/ level
+-- * Unidirectional resolution only ( one pass )
+--
+data Fqn
+   = ClassInstance ClassInstanceContent
+   | ClassName ClassNameContent
+   | FieldedAccess Fqn Token.FieldName
+   | CallMethodOfClass Location String Token.ClassName
+   | FirstPartyImport FirstPartyImportContent
+   | ThirdPartyImport ThirdPartyImportContent
+   | NativeTypeInt
+   | NativeTypeString
+   | NativeTypeBool
+   | NativeTypeConstInt Int
+   | NativeTypeConstStr String
+   | NativeTypeConstBool Bool
+   | NativeTypeNull
+   | Unknwon
+   deriving ( Show, Eq, Ord, Generic, ToJSON, FromJSON )
 
-any :: Fqn
-any = Fqn { content = "any" }
+data FirstPartyImportContent
+   = FirstPartyImportContent
+     {
+         firstPartyImportedLocation :: FilePath,
+         firstPartyImportedName :: Maybe String
+     }
+     deriving ( Show, Eq, Ord, Generic, ToJSON, FromJSON )
 
-nativeInt :: Fqn
-nativeInt = Fqn { content = "int" }
+-- |
+--
+-- ==== __Examples:__
+--
+-- * Entire third party package import
+--
+-- @
+-- # thirdPartyImportedPackageName == "urllib3"
+-- # thirdPartyImportedPartsFromPackage == []
+-- # thirdPartyImportJustOneName == Nothing
+-- # thirdPartyImportWithAlias == Nothing
+-- import urllib3
+-- @
+--
+-- * Specific third party import
+--
+-- @
+-- # thirdPartyImportedPackageName == "scipy"
+-- # thirdPartyImportedPartsFromPackage == [ "linalg" ]
+-- # thirdPartyImportJustOneName == Nothing
+-- # thirdPartyImportWithAlias == Nothing
+-- import scipy.linalg
+-- @
+--
+-- * Named third party import
+--
+-- @
+-- # thirdPartyImportedPackageName == "django"
+-- # thirdPartyImportedPartsFromPackage == [ "db" ]
+-- # thirdPartyImportJustOneName == "models"
+-- # thirdPartyImportWithAlias == Nothing
+-- from django.db import models
+-- @
+--
+data ThirdPartyImportContent
+   = ThirdPartyImportContent
+     {
+         thirdPartyImportedPackageName :: String,
+         thirdPartyImportedPartsFromPackage :: [ String ],
+         thirdPartyImportJustOneName :: Maybe String,
+         thirdPartyImportWithAlias :: Maybe String
+     }
+     deriving ( Show, Eq, Ord, Generic, ToJSON, FromJSON )
 
-nativeStr :: Fqn
-nativeStr = Fqn { content = "str" }
+data ClassInstanceContent
+   = ClassInstanceContent
+     {
+         instanceName :: InstanceName,
+         instanceOfClass :: Token.ClassName
+     }
+     deriving ( Show, Eq, Ord, Generic, ToJSON, FromJSON )
 
-nativeBool :: Fqn
-nativeBool = Fqn { content = "bool" }
+data InstanceName
+   = Self -- ^ Python, Ruby
+   | This -- ^ Java, Php, Javascript, Typescript
+   deriving ( Show, Eq, Ord, Generic, ToJSON, FromJSON )
 
-nativeNull :: Fqn
-nativeNull = Fqn { content = "null" }
+data InstanceAttribute
+   = InstanceAttribute
+     {
+         attributeName :: Token.FieldName
+     }
+     deriving ( Show, Eq, Ord, Generic, ToJSON, FromJSON )
+
+data ClassNameContent
+   = ClassNameContent
+     {
+         classNameContent :: Token.ClassName
+     }
+     deriving ( Show, Eq, Ord, Generic, ToJSON, FromJSON )
